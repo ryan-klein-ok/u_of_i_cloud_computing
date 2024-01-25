07f3edf1d41d43d7e5a866b663a8e5e25ff170c4
ryan-klein-ok <139692705+ryan-klein-ok@users.noreply.github.com>
2024-01-25 13:57:41 -0600

    Update serve.py

--- serve.py
+++ serve.py
@@ -15,7 +15,7 @@ def handle_requests():
         return jsonify({'private_ip': private_ip})
 
 def start_cpu_stress():
-    subprocess.Popen(['python', 'stress_cpu.py'])
+    subprocess.Popen(['python3', 'stress_cpu.py'])
 
 def get_private_ip():
     private_ip = socket.gethostbyname(socket.gethostname())
